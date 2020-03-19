2340fbcbeb8f4568872d78dd8459ae053afa5aee
guojunjie <121785021@qq.com>
2020-03-19 19:44:57 +0800

    first commit 2020.3.19

---
+++ CV/cv_2.py
@@ -0,0 +1,11 @@
+import cv2
+vidcap = cv2.VideoCapture('video/video.avi')
+success,image = vidcap.read()
+count = 0
+success = True
+while success:
+    success,image = vidcap.read()
+    cv2.imwrite("pic/frame%d.jpg" % count, image)  # save frame as JPEG file
+    if cv2.waitKey(10) == 27:
+        break
+    count += 1
